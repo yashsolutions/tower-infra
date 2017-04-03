021e08210de4a65f308b8b7d662e867b9c432899
ashish.nanotkar <ashish.nanotkar@srkay.com>
2017-04-03 17:43:10 +0530

    debugging ansible server code

--- message_bridge/run_ansible.sh
+++ message_bridge/run_ansible.sh
@@ -5,6 +5,6 @@ message_json=`echo $3 | base64 --decode`
 while read -r line
 do
 	/home/ansible/ansible/message_bridge/log_to_redis.py $1 log "$line"
-done < <(cd /home/ansible/ansible && ansible-playbook -e "$message_json" "playbooks/$2.yml" 2>&1)
+done < <(cd /home/ansible/ansible && ansible-playbook --vault-password-file /home/ansible/.ansible-secret -e "$message_json" "playbooks/$2.yml" 2>&1)
 
 /home/ansible/ansible/message_bridge/log_to_redis.py $1 end 0
